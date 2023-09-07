17931f2a5215b5e2775f2dda346a1eaaa22d2f0a
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-09-07 20:03:53 +0100

    a class Rectangle that defines a rectangle with instance attributes creating and empty object updated

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,14 +3,19 @@
 class Rectangle {
 
 	constructor(w, h) {
-		if ((w <= 0) || (h <= 0) || !Number.isInteger(w) || !Number.isInteger(h)) {
-			this.width = undefined;
-			this.height = undefined;
-		}
-		else {
-			this.width = w;
-			this.width = h;
-		}
+		// if ((w <= 0) || (h <= 0) || !Number.isInteger(w) || !Number.isInteger(h)) {
+		// 	this.width = undefined;
+		// 	this.height = undefined;
+		// }
+		// else {
+		// 	this.width = w;
+		// 	this.width = h;
+		// }
+		if ((w <= 0) || (h <= 0) || !h || !w) {
+            return this;
+          }
+        this.width = w;
+        this.height = h;
 	}
 }
 
