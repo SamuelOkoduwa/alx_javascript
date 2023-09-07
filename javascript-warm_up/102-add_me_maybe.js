a77dec7df31499484c9e0a121bda45dd86422dcc
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-09-07 17:48:34 +0100

    a function that increments and calls a function

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!bin/usr/node 
 
-exports.incrementAndCall = function incrementAndCall(number, theFunction) {
+exports.incrementAndCall = function (number, theFunction) {
 	// Increment the number
 	number++;
   
