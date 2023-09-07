5cdc4238e1eecc749b63a84100f4ccd11d5ec00a
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-09-07 17:22:59 +0100

    Write a function that executes x times a function updated

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -10,18 +10,22 @@
 
 //   module.export.callMeMeMoby = callMeMeMoby;
 
-const executeXTimes = {
-	execute: function(x, theFunction) {
-	  for (let i = 0; i < x; i++) {
-		theFunction();
-	  }
-	},
-  };
+// const executeXTimes = {
+// 	execute: function(x, theFunction) {
+// 	  for (let i = 0; i < x; i++) {
+// 		theFunction();
+// 	  }
+// 	},
+//   };
   
-  // Now you can use the execute function from outside
-  executeXTimes.execute(5, () => {
-	console.log("This function will execute 5 times.");
-  });
+//   // Now you can use the execute function from outside
+//   executeXTimes.execute(5, () => {
+// 	console.log("This function will execute 5 times.");
+//   });
 
   
-  
+exports.callMeMoby = function (x, theFunction){
+    for (let i = 0; i < x; i++){
+        theFunction();
+    }
+}
