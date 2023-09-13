b1add10ec7c58a2abbdacad06797d00275bb1964
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-09-13 17:50:00 +0100

    A function that takes two argument with a nested function to display the two arguments of the parent function

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-function welcome(firstName, lastName) {
-	let fullName = firstName + ' ' + lastName;
-	function displayFullName(){
-		alert('welcome' + ' ' + fullName + '!');
+function welcome(firstName, lastName) { 	//The parent function
+	let fullName = firstName + ' ' + lastName;	//Variable for the first and last Name
+	function displayFullName(){		//Nested function to display the two arguments, first and last name
+		alert('welcome' + ' ' + fullName + '!');	//the alert to be displayed
 	}
-	displayFullName();
+	displayFullName();	//the call back function
 }
