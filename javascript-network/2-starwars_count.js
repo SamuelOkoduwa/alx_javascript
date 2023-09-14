a3e46e429ee1bd94eac1e543d9d83b2ec3b9b317
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2023-09-14 21:02:00 +0100

    A script that prints the number of movies where the character 'Wedge Antiles' is present

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,26 @@
+
+const apiURL = "https://swapi-api.alx-tools.com/api/films/";
+const characterID = 18;
+
+fetch(apiURL)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Filter films where Wedge Antilles appears
+    const filmsWithWedge = data.results.filter((film) =>
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterID}/`)
+    );
+
+    // Count the number of films with Wedge Antilles
+    const numFilms = filmsWithWedge.length;
+
+    // Print the result
+    console.log((numFilms));
+  })
+  .catch((error) => {
+    console.error(`Error: ${error.message}`);
+  });
